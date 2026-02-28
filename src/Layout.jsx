b215f0a28abc865860bacddfad0a2ac1a96c447c
Are Kjetil <kjetilare327@gmail.com>
2026-02-28 12:28:02 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -320,7 +320,6 @@ function InnerLayout({ children, currentPageName }) {
       }
 
       const NO_LAYOUT_PAGES = ['Onboarding', 'GdprConsent', 'TermsOfService'];
-      const NO_AUTH_PAGES = ['Onboarding', 'GdprConsent', 'TermsOfService'];
 
       function BootLoader() {
         return (
