afdbc8a36d9b0a2decc14562bbef3af396af6178
Are Kjetil <kjetilare327@gmail.com>
2026-03-03 01:18:08 +0000

    File changes

--- src/pages/Budget.jsx
+++ src/pages/Budget.jsx
@@ -246,8 +246,8 @@ export default function BudgetPage() {
         ))}
       </div>
 
-      {isAdmin && <Dialog open={showForm} onOpenChange={setShowForm}>
-
+      {isAdmin && (
+      <Dialog open={showForm} onOpenChange={setShowForm}>
         <DialogContent className="max-w-md">
           <DialogHeader>
             <DialogTitle>{editData ? 'Rediger budsjettpost' : 'Ny budsjettpost'}</DialogTitle>
